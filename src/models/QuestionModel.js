844f5382786abef606ffaf0ced0977d60f5f1ed6
Mustafa Eltoum <57389533+Xlizer1@users.noreply.github.com>
2023-01-29 12:02:51 +0300

    Update QuestionModel.js

--- src/models/QuestionModel.js
+++ src/models/QuestionModel.js
@@ -5,7 +5,9 @@ const { Schema, model } = pkg;
 const QuestionSchema = new Schema({
   title: String,
   answer: String,
-  keyWord: String,
+  material: String,
+  year: String,
+  trun: String,
   youtubeLink: String,
 });
 
